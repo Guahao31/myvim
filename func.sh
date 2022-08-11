0a5b4e8a938cd669c651ab01c1d6859082424188
Guahao <guahao@zju.edu.cn>
2022-08-11 10:37:54 +0800

    Updated: Print cursor at the end of curr line

--- func.sh
+++ func.sh
@@ -8,11 +8,17 @@ print_page() {
         if [ $curr_row -ne $cur_pos_row ]; then
             echo "$one_line"
         else
-            # 通过格式化输出，将光标处的背景调为浅色
-            echo -n "${one_line:0:$cur_pos_col}" # -n 避免echo添加换行
-            echo -e -n "\e[100m${one_line:$cur_pos_col:1}" # -e用来打开echo对转义符号的开关
-            cur_next_col=$cur_pos_col+1
-            echo -e "\e[0m${one_line:$cur_next_col}" # 转为正常前景背景，输出光标后边的内容
+            if [ $cur_pos_col -ne ${#one_line} ]; then
+                # 通过格式化输出，将光标处的背景调为浅色
+                echo -n "${one_line:0:$cur_pos_col}" # -n 避免echo添加换行
+                echo -e -n "\e[100m${one_line:$cur_pos_col:1}" # -e用来打开echo对转义符号的开关
+                cur_next_col=$cur_pos_col+1
+                echo -e "\e[0m${one_line:$cur_next_col}" # 转为正常前景背景，输出光标后边的内容
+            else
+                # 光标在行末
+                echo -n "$one_line"
+                echo -e "\e[100m \e[0m" # 在行末打印空格，表示光标所在位置
+            fi
         fi
             ((++curr_row)) # 行数计数器自增1
     done < "$temp_file"
--- myvim.sh
+++ myvim.sh
@@ -15,13 +15,19 @@ temp_file=.$1.my_tmp
 # 准备建立编辑暂存使用的文件 形式为 file_name.my_tmp
 # 检查当前是否存在暂存文件，如果存在，证明其他myvim正在编辑本文本
 # 报错并退出
-if [ -e $temp_file ]; then
-    echo "Some other myvim is editing $file_name."
-    exit 1
-fi
+# if [ -e $temp_file ]; then
+#     echo "Some other myvim is editing $file_name."
+#     exit 1
+# fi
+
+curr_line="" # 记录光标当前所在的行的内容
 # 如果文件存在，则将内容复制到临时文件中，如果文件不存在，则直接新建一个临时文件
 if [ -e $file_name ]; then
     cp $file_name $temp_file # 拷贝获得临时文件
+    # 获取第一行内容到curr_line
+    # $()获取指令输出到环境变量
+    # cat file_name带行数打印文件内容通过管道传给sed，-n选取第一行传给curr_line
+    curr_line=$(cat $temp_file | sed -n '1p')
 else
     touch $temp_file # 文件不存在，直接新建一个临时文件
 fi
@@ -30,11 +36,28 @@ fi
 cur_pos_col=0
 cur_pos_row=0
 
-# 清除屏幕并准备打印文件内容
-clear # 清屏
 tput civis # 取消终端光标的显示
+# 初始打印
+clear; print_page
 
-print_page 
+# 主体部分，退出前一直循环，每次处理一个字符的输入
+while read -s -N1 input; do
+    # -s 不将用户的输入反映到终端输出；-N1 每次读入1个字符
+    
+    # 清除屏幕并准备打印文件内容
+    clear # 清屏
+    print_page # 打印内容
+
+    # 获得目前处理的行内容
+    curr_line=$(cat $temp_file | sed -n $((curr_pos_row+1))'p')
+    
+    # 处理输入
+    modify_line=${curr_line:0:$cur_pos_col}$input${curr_line:$((cur_pos_col+1))}
+    sed -i $((cur_pos_row+1))'s/.*/'${modify_line}'/' ${temp_file}
+    cur_pos_col=$((cur_pos_col+1)) # 移动光标到下一个字符
+
+done
+ 
 
 # set | tail # 查看文件所有的参数内容
 
